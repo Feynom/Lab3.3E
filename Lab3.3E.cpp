bf33a2e75fa439c5a894c1d6af20027b7db27064
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-23 18:55:07 +0200

    Add files via upload

---
+++ Lab3.3E.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include "Fraction.h"
+
+int main()
+{
+    cout << "Number of objects of class Fraction: " << Object::Count() << endl;
+    Fraction a;
+    cout << "Number of objects of class Fraction: " << Object::Count() << endl;
+    Fraction b;
+    cout << "Number of objects of class Fraction: " << Object::Count() << endl;
+    Fraction c;
+    cout << "Number of objects of class Fraction: " << Object::Count() << endl;
+    int result;
+
+    cout << "\nPair a" << endl;
+    cin >> a;
+    cout << a;
+
+    cout << "\nPair b" << endl;
+    cin >> b;
+    cout << b;
+
+    cout << "\nPair c" << endl;
+    cin >> c;
+    cout << c;
+
+    cout << "\na and b" << endl;
+    result = a.ComparePairs(a, b);
+    a.ComparePairsResult(result);
+
+    cout << "\na and c" << endl;
+    result = a.ComparePairs(a, c);
+    a.ComparePairsResult(result);
+
+    cout << "\nb and c" << endl;
+    result = a.ComparePairs(b, c);
+    a.ComparePairsResult(result);
+
+    cout << "a++" << endl;
+    cout << a++ << endl;
+
+    cout << "a--" << endl;
+    cout << a-- << endl;
+
+    cout << "++a" << endl;
+    cout << ++a << endl;
+
+    cout << "--a" << endl;
+    cout << --a << endl;
+
+    cout << "Size of class is equal to " << sizeof(Fraction) << endl;
+
+    return 0;
+}
---
+++ UnitTest3.3E.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab3.3E/Lab3.3E/Fraction.h"
+#include "../Lab3.3E/Lab3.3E/Fraction.cpp"
+#include "../Lab3.3E/Lab3.3E/Pair.h"
+#include "../Lab3.3E/Lab3.3E/Pair.cpp"
+#include "../Lab3.3E/Lab3.3E/Object.h"
+#include "../Lab3.3E/Lab3.3E/Object.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest33E
+{
+	TEST_CLASS(UnitTest33E)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Fraction a(1, 5), b(1, 5);
+			Assert::AreEqual(a == b, true);
+		}
+	};
+}
